4cc7611bab9ac9a261139c2c0b01ef824d7a1c72
BorisLechev <b.k.lechev@gmail.com>
2018-11-26 21:58:26 +0200

    Added lab exercises.

---
+++ 2. AJAX and jQuery AJAX/Lab/3. Github Repos/repos.js
@@ -0,0 +1,19 @@
+function loadRepos() {
+    $('#repos').empty();
+    $.ajax({
+        method: "GET",
+        url: `https://api.github.com/users/${$('#username').val()}/repos`,
+        success: handleSuccess,
+        error: handleError
+    });
+
+    function handleSuccess(res) {
+        for (let repo of res) {
+            $('<li>').append($(`<a href="${repo.html_url}">${repo.full_name}</a>`)).appendTo($('#repos'));
+        }
+    }
+
+    function handleError(err) {
+        console.log(err);
+    }
+}
